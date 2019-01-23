0b022907950237a230ed3476c71496c46398d842
taras <taras.demyanyshyn@gmail.com>
2019-01-23 15:04:59 +0200

    Исправлена ошибка, полученная в результате спешки)))

--- lesson-1/weight.rb
+++ lesson-1/weight.rb
@@ -2,6 +2,6 @@ puts "Введите Ваше имя"
 user_name = gets.chomp
 puts "Ваш рост?"
 ideal_weight = -110 + weight= gets.chomp.to_i 
-puts "#{name}, Ваш идеальный вес #{ideal_weight}! "if ideal_weight > 0
-puts "#{name}, Ваш вес идеален!" if ideal_weight == 0
-puts "Уважаемый #{name}, Вам бы подрасти на #{ideal_height = ideal_weight.abs.to_f} cm!" if ideal_weight < 0
+puts "#{user_name}, Ваш идеальный вес #{ideal_weight}!" if ideal_weight > 0
+puts "#{user_name}, Ваш вес идеален!" if ideal_weight == 0
+puts "Уважаемый #{user_name}, Вам бы подрасти на #{ideal_height = ideal_weight.abs.to_f} cm!" if ideal_weight < 0
