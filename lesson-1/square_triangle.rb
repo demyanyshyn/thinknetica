baa5366c39fddb855823013402eefdcf892afc69
taras <taras.demyanyshyn@gmail.com>
2019-01-24 18:36:46 +0200

    Задание 1: Исправлено

--- lesson-1/square_triangle.rb
+++ lesson-1/square_triangle.rb
@@ -1,5 +1,5 @@
 puts "Введите высоту треугольника:"
-h_triangle = gets.chomp.to_f
+h_triangle = gets.to_f
 puts "Введите длину основании треугольника:"
-a_triangle = gets.chomp.to_f
+a_triangle = gets.to_f
 puts "Площадь треугольника: #{1.0 / 2 * a_triangle * h_triangle}!"
